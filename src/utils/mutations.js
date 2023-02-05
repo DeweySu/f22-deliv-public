d22fb5739475ed086ad6a8c35cf47148e36b42dc
Dewey Su <112769574+DeweySu@users.noreply.github.com>
2023-02-05 00:30:22 -0500

    Update mutations.js

--- src/utils/mutations.js
+++ src/utils/mutations.js
@@ -1,4 +1,4 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, updateDoc, deleteDoc, collection, doc, FieldValue } from "firebase/firestore";
 import { db } from './firebase';
 
 // Functions for database mutations
@@ -9,6 +9,7 @@ export const emptyEntry = {
    description: "",
    user: "",
    category: 0,
+   clicks: 0
 }
 
 export async function addEntry(entry) {
@@ -18,16 +19,34 @@ export async function addEntry(entry) {
       description: entry.description,
       user: entry.user,
       category: entry.category,
+      clicks: entry.clicks,
       // The ID of the current user is logged with the new entry for database user-access functionality.
       // You should not remove this userid property, otherwise your logged entries will not display.
-      userid: entry.userid,
+      userid: entry.userid
    });
 }
 
-export async function updateEntry(entry) {
+export async function updateEntry(entry, entryID) {
    // TODO: Create Mutation to Edit Entry
+
+   await updateDoc(doc(db, "entries", entryID), {
+      name: entry.name,
+      link: entry.link,
+      description: entry.description,
+      user: entry.user,
+      category: entry.category
+   });
+
 }
 
-export async function deleteEntry(entry) {
+export async function deleteEntry(entryID) {
    // TODO: Create Mutation to Delete Entry
+   await deleteDoc(doc(db, "entries", entryID));
+}
+
+export async function clickEntry(entryID, numClicks, link) {
+   await updateDoc(doc(db, "entries", entryID), {
+      clicks: numClicks + 1
+   });
+   window.location.href = link;
 }
